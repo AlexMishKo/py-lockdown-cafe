95636cbebc3daf658c0a9c613daba43a49a2f5cc
Oleksii Mishchenko <alex3hard@gmail.com>
2025-11-07 18:05:55 +0200

    added name = visitor in cape.py to avoid double quotes for flake8 validation

--- app/cafe.py
+++ app/cafe.py
@@ -9,16 +9,17 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
+        name = visitor["name"]
         if "vaccine" not in visitor:
-            raise NotVaccinatedError(f"{visitor["name"]} is not vaccinated.")
+            raise NotVaccinatedError(f"{name} is not vaccinated.")
 
         expiration_date = visitor["vaccine"]["expiration_date"]
         if expiration_date < datetime.date.today():
             raise OutdatedVaccineError(f"Vaccine of "
-                                       f"{visitor["name"]} is expired.")
+                                       f"{name} is expired.")
 
         if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError(f"{visitor["name"]} "
+            raise NotWearingMaskError(f"{name} "
                                       f"is not wearing a mask.")
 
         return f"Welcome to {self.name}"
