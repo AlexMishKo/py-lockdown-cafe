f6c3b22a1ae4413936011243ba88e439c3d58193
Oleksii Mishchenko <alex3hard@gmail.com>
2025-11-07 18:24:25 +0200

    using get method 2.0

--- app/cafe.py
+++ app/cafe.py
@@ -1,7 +1,9 @@
 import datetime
-from app.errors import (NotVaccinatedError,
-                        OutdatedVaccineError,
-                        NotWearingMaskError)
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError,
+)
 
 
 class Cafe:
@@ -10,16 +12,20 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         name = visitor["name"]
-        if "vaccine" not in visitor:
+
+        vaccine = visitor.get("vaccine")
+        if not vaccine:
             raise NotVaccinatedError(f"{name} is not vaccinated.")
 
-        expiration_date = visitor["vaccine"]["expiration_date"]
+        expiration_date = vaccine.get("expiration_date")
+        if not expiration_date:
+            raise OutdatedVaccineError(f"Missing expiration date"
+                                       f" for {name}'s vaccine.")
+
         if expiration_date < datetime.date.today():
-            raise OutdatedVaccineError(f"Vaccine of "
-                                       f"{name} is expired.")
+            raise OutdatedVaccineError(f"Vaccine of {name} is expired.")
 
         if not visitor.get("wearing_a_mask"):
-            raise NotWearingMaskError(f"{name} "
-                                      f"is not wearing a mask.")
+            raise NotWearingMaskError(f"{name} is not wearing a mask.")
 
         return f"Welcome to {self.name}"
